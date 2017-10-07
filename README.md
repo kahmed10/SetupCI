a25e56d59d0aa71000e0b7115d098a5dbccfa1bf
kahmed10 <kahmed10@illinois.edu>
2017-10-07 08:09:32 -0500

    Reorganized structure

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the initial phase of SetupCI, a tool that performs regression testing on installation guides. Current support will begin with GitHub projects that have a .md file to parse.
---
+++ src/file_parser.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <regex>
+
+
+using namespace std;
+
+
+
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Pass in clean .md file with only comments" < endl;
+	}
+	return 0;
+}
