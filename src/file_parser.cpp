010ff69c3f22d86c16352830b778ee7eaa1b4315
kahmed10 <kahmed10@illinois.edu>
2017-10-29 16:09:53 -0500

    Updated file_parser

--- src/file_parser.cpp
+++ src/file_parser.cpp
@@ -12,17 +12,34 @@ int main(int argc, char* argv[])
 	if (argc != 2)
 	{
 		cout << "Pass in clean .md file" << endl;
-    //    return -1;
+        return -1;
 	}
 
     UbuntuCall ubuntuCaller;
+    string line;
+    vector<string> fileList;
+    ifstream inFile(argv[1]);
 
-    // currently testing if CIs can be parsed correctly
-    string tests[2] = {"CI_RUN_UBUNTU(ls)","CI_RUN_UBUNTU()"};
-
-    for (auto test: tests)
+    if (inFile.is_open())
     {
-    	ubuntuCaller.Search(test);
+        while(getline(inFile,line))
+        {
+            fileList.push_back(line);
+        }
+        inFile.close();
+    }
+
+    else
+    {
+        cout << "Error: Unable to open file: " << argv[1] << endl;
+        return -1;
+    }
+
+    
+
+    for (auto entry: fileList)
+    {
+    	ubuntuCaller.Search(entry);
     }
     vector<string> results = ubuntuCaller.GetCommands();
 
